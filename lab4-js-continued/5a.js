f0519ccaad7cc28bd8b350a0611751d3e730592d
Ahmad Alaziz <61874844+s19047@users.noreply.github.com>
2020-11-20 08:58:24 +0100

    Update 5a.js

--- lab4-js-continued/5a.js
+++ lab4-js-continued/5a.js
@@ -1,1 +1,58 @@
 
+const human = {
+    firstName: "Ahmad",
+    lastName: "Alaziz",
+    dob: new Date('2000-09-15'),
+
+    caluclateAge: function () { 
+        let today = new Date();
+        let age = today.getFullYear() - this.dob.getFullYear(); 
+        let month = today.getMonth() - this.dob.getMonth();
+        let day = today.getDate() - this.dob.getDate();
+        if(month<0){
+              console.log(`${this.firstName} is ${--age} years old!`);
+        }else if(month === 0){
+            if(day>0){
+                console.log(`${this.firstName}'s ${age} birthday was ${day} days ago`);
+            }else if(day < 0){
+                console.log(`${this.firstName}'s ${age} birthday is in ${-day} days!`);
+            }else{
+                console.log(`${this.firstName}'s ${age} birthday is today!!`);
+            }
+        }else{
+             console.log(`${this.firstName} is ${age} years old!`);
+        }},
+
+    goToSleep: function(){
+        console.log("sleeping")
+    },
+
+    eat: function(){
+        console.log("eating")
+    }
+};
+
+human.caluclateAge();
+
+console.log("\n\n----Printing properties and methods----")
+function printHumanProperties(human){
+    let propValue;
+    for(let propName in human){
+        propValue = human[propName]
+        console.log(propName,propValue,typeof(propValue));
+    }
+}
+printHumanProperties(human)
+
+console.log("\n\n----Printing properties without methods----")
+function printHumanProperties2(human){
+    let propValue, propType;
+    for(let propName in human){
+        propValue = human[propName];
+        propType = typeof(propValue);
+        if(propType != "function"){
+            console.log(propName,propValue,propType)
+        };
+    }
+}
+printHumanProperties2(human)
